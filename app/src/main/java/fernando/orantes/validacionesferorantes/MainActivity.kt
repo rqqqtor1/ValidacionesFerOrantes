2d07d0069d18ba51a4b4f2e1eedbbb50f9adff60
- <->
2024-06-07 14:20:31 -0600

    validaciones semi hechas

--- app/src/main/java/fernando/orantes/validacionesferorantes/MainActivity.kt
+++ app/src/main/java/fernando/orantes/validacionesferorantes/MainActivity.kt
@@ -1,6 +1,10 @@
 package fernando.orantes.validacionesferorantes
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,45 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //1- Mandar a llamar todos los elementos de la vista
+
+        val txtName = findViewById<EditText>(R.id.txtName)
+        val txtEmail = findViewById<EditText>(R.id.txtEmail)
+        val txtPassword = findViewById<EditText>(R.id.txtPassword)
+        val txtDUI = findViewById<EditText>(R.id.txtDUI)
+        val txtEdad = findViewById<EditText>(R.id.txtEdad)
+        val btnRegistrarse= findViewById<Button>(R.id.btnRegistrarse)
+
+        //Hay que programar el boton chero
+        btnRegistrarse.setOnClickListener{
+
+            //Se valida que ningun campo este vacio
+            if (txtName.text.isEmpty()  || txtEmail.text.isEmpty() || txtEdad.text.isEmpty() || txtDUI.text.isEmpty() ||
+                txtPassword.text.isEmpty()) {
+                Toast.makeText(this, "Please complete all fields", Toast.LENGTH_SHORT).show()
+
+            }
+            else {
+                //Comprobar que solo ingrese numeros
+                if (!txtEdad.text.matches("[0-9]+".toRegex())) {
+                    Toast.makeText(this, "Enter numbers", Toast.LENGTH_SHORT).show()
+
+                }
+
+                else {
+                    //Validar el correo
+                    if (!txtEmail.text.matches("[a-zA-Z0-9.-_]+@[a-z]+\\.[a-z]".toRegex()))
+                        Toast.makeText( this, "Enter valid email", Toast.LENGTH_SHORT).show()
+                }
+
+
+
+
+            }
+
+
         }
     }
 }
