ba142d8c9d80886e2ba01adf532b87f86a6ec5d1
- <->
2024-06-07 15:15:08 -0600

    agregar funciones de splash

--- app/src/main/java/fernando/orantes/validacionesferorantes/splashscreen.kt
+++ app/src/main/java/fernando/orantes/validacionesferorantes/splashscreen.kt
@@ -1,10 +1,15 @@
 package fernando.orantes.validacionesferorantes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,20 @@ class splashscreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //creamos una corrutina
+
+        GlobalScope.launch(Dispatchers.Main){
+
+            delay(3000)
+            //Iniciar la actividad
+
+            val pantallaInicio = Intent( this@splashscreen, MainActivity::class.java)
+            startActivity(pantallaInicio)
+
+            //finalizar
+            finish()
         }
     }
 }
